f649052e5e2fc1212b9ddcb73282b69e62a5fe71
F.Lofgren <fredrik.lofgren@omegapoint.se>
2020-09-24 12:49:17 +0200

    Completed part 1

--- src/main/java/se/omegapoint/refactoring_lab/movieshop/Customer.java
+++ src/main/java/se/omegapoint/refactoring_lab/movieshop/Customer.java
@@ -20,43 +20,37 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            // determine amount for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for two days new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
-
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
-        }
+        result += getRentalFigures();
         // add footer line
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "Amount owed is " + getTotalCharge() + "\n";
+        result += "You earned " + getTotalPoints() + " frequent renter points";
         return result;
     }
+
+    private String getRentalFigures() {
+        String tmpResult = "";
+        for (Rental each : rentals) {
+            // show figures for this rental
+            tmpResult += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.charge()) + "\n";
+        }
+        return tmpResult;
+    }
+
+    private int getTotalPoints() {
+        int tmpPoints = 0;
+        for (Rental each : rentals) {
+            tmpPoints += each.points();
+        }
+        return tmpPoints;
+    }
+
+    private double getTotalCharge() {
+        double tmpAmount = 0;
+        for (Rental each : rentals) {
+            tmpAmount += each.charge();
+        }
+        return tmpAmount;
+    }
+
 }
