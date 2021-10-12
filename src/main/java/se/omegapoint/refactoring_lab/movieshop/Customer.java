1b619bd7f7e158283daa7138553731ecaa56b5dd
Victoria Catalán <victoria.catalan@omegapoint.se>
2021-10-12 18:40:45 +0200

    add braces to if statements and remove explicit list type

--- src/main/java/se/omegapoint/refactoring_lab/movieshop/Customer.java
+++ src/main/java/se/omegapoint/refactoring_lab/movieshop/Customer.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Customer {
     private String name;
-    private List<Rental> rentals = new ArrayList<Rental>();
+    private List<Rental> rentals = new ArrayList<>();
 
     public Customer(String name) {
         this.name = name;
@@ -23,29 +23,32 @@ public class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
-        for(Rental each : rentals) {
+        for (Rental each : rentals) {
             double thisAmount = 0;
             // determine amount for each line
             switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
+                    if (each.getDaysRented() > 2) {
                         thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
                     break;
                 case Movie.NEW_RELEASE:
                     thisAmount += each.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if(each.getDaysRented() > 3)
+                    if (each.getDaysRented() > 3) {
                         thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
                     break;
             }
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for two days new release rental
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
                 frequentRenterPoints++;
+            }
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
